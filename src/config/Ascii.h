8df103a14a5ffe63b09101f936e23094524ecc4c
deanb1982 <deanb1982@gmail.com>
2024-02-05 23:05:58 -0500

    updates

--- src/config/Ascii.h
+++ src/config/Ascii.h
@@ -7,22 +7,22 @@
 #include <string>
 
 const std::string windowsAsciiArt =
-"\n                       /n"
-" _____    _____   _____ /n"
-"|  __ \  / ____| / ____|/n"
-"| |__) || |     | |     /n"
-"|  _  / | |     | |     /n"
-"| | \ \ | |____ | |____ /n"
-"|_|  \_\ \_____| \_____|/n";
+"\n                       \n"
+" _____    _____   _____ \n"
+"|  __ \  / ____| / ____|\n"
+"| |__) || |     | |     \n"
+"|  _  / | |     | |     \n"
+"| | \ \ | |____ | |____ \n"
+"|_|  \_\ \_____| \_____|\n";
 
 const std::string nonWindowsAsciiArt =
-   "\n                            /n"
-    "  ______    ______   ______ /n"
-    " (_____ \  / _____) / _____)/n"
-    "  _____) )| /      | /      /n"
-    " (_____ ( | |      | |      /n"
-    "       | || \_____ | \_____ /n"
-    "       |_| \______) \______)/n";
+   "\n                            \n"
+    "  ______    ______   ______ \n"
+    " (_____ \  / _____) / _____)\n"
+    "  _____) )| /      | /      \n"
+    " (_____ ( | |      | |      \n"
+    "       | || \_____ | \_____ \n"
+    "       |_| \______) \______)\n";
 
 /* Windows has some characters it won't display in a terminal. If your ascii
    art works fine on Windows and Linux terminals, just replace 'asciiArt' with
